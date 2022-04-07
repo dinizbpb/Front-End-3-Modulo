c52215b28f9a6c2947739237704e7c8e1662eeb7
dinizbpb <98328460+dinizbpb@users.noreply.github.com>
2022-04-06 23:31:25 -0300

    CRUD PROJETO INTEGRADOR

---
+++ Lista 06-04/Unidas/Unidas/src/app/cadastrar/cadastrar.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from '../model/Usuario';
+import { AlertasService } from '../service/alertas.service';
+import { AuthService } from '../service/auth.service';
+
+@Component({
+  selector: 'app-cadastrar',
+  templateUrl: './cadastrar.component.html',
+  styleUrls: ['./cadastrar.component.css']
+})
+export class CadastrarComponent implements OnInit {
+
+  usuario: Usuario= new Usuario;
+
+confirmSenha:string;
+
+tipoUsuario:string='normal';
+
+
+  constructor(
+    private authService:AuthService,
+    private router: Router,
+    private alertas:AlertasService
+    ) { }
+
+  ngOnInit(): void {
+  window.scroll(0,0);
+  }
+
+confirmarSenha(event:any){
+this.confirmSenha=event.target.value
+}
+
+
+
+cadastrarUsuario() {
+
+
+  if (this.confirmSenha === this.usuario.senha) {
+
+    this.usuario.tipo=this.tipoUsuario;
+
+    this.authService.Cadastrar(this.usuario).subscribe((resp: Usuario) => {
+      this.usuario = resp;
+      this.router.navigate(['/entrar']);
+      this.alertas.showAlertSuccess('Usuário cadastrado com sucesso!');
+    });
+  } else {
+    this.alertas.showAlertDanger('As senha não estão iguais.');
+  }
+}
+}
